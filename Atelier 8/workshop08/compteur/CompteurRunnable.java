01120c862811d04799728441c134ac86ca8ddf00
julien-remmery-vinci <julien.remmery@student.vinci.be>
2023-11-09 09:46:20 +0100

    update: Atelier 8 ex3.2

--- Atelier 8/workshop08/compteur/CompteurRunnable.java
+++ Atelier 8/workshop08/compteur/CompteurRunnable.java
@@ -6,7 +6,7 @@ public class CompteurRunnable implements Runnable {
     private int max;
 
     //TODO: ajouter un attribut de classe qui retient l'ordre d'arrivée.
-    private static int ordreArrivee = 0;
+    private static Integer ordreArrivee = 0;
 
     @Override
     public void run() {
@@ -14,15 +14,18 @@ public class CompteurRunnable implements Runnable {
         //         A chaque incrémentation, afficher le nom du compteur et son compte actuel.
         //      2. Quand le compte est terminé, afficher que le compteur a finit de compter
         //         et indiquer son ordre d'arrivée.
-        for (int i = 0; i < max; i++) {
-            System.out.println(nom + ": "+ i);
-            try{
+        for (int i = 0; i <= max; i++) {
+            System.out.println(nom + ": " + i);
+            if(i==max) break;
+            try {
                 Thread.sleep(10);
-            }catch (InterruptedException e){
+            } catch (InterruptedException e) {
                 System.out.println(e);
             }
         }
-        System.out.println(nom+" a fini de compter en position "+ ++ordreArrivee);
+        synchronized (ordreArrivee){
+            System.out.println(nom + " a fini de compter en position " + ++ordreArrivee);
+        }
     }
 
     public CompteurRunnable(String nom, int max) {
